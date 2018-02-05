930917cc69777cf3d8de819a7382577fb6353644
VickyMutai <vicky.mutai96@gmail.com>
2018-02-05 20:04:44 +0300

    create class to update profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
+
 class PitchForm(FlaskForm):
     name = StringField('Authors Name', validators=[Required()])
     category = StringField('Pitch Category', validators=[Required()])
     pitch = TextAreaField('Pitch', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
