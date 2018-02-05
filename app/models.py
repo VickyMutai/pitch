5118c27549bea8de2a5b6225873b5a7fa74e806e
VickyMutai <vicky.mutai96@gmail.com>
2018-02-05 23:12:44 +0300

    create methods for pitch

--- app/models.py
+++ app/models.py
@@ -42,25 +42,20 @@ class Pitch(db.Model):
     posted = db.Column(db.DateTime,default=datetime.utcnow)
     user_id=db.Column(db.Integer,db.ForeignKey("users.id"))
 
-
-
-    all_pitch = []
     def __init__(self,name,category,pitch):
         self.name = name
         self.category = category
         self.pitch = pitch
 
     def save_pitch(self):
-        Pitch.all_pitch.append(self)
+        db.session.add(self)
+        db.session.commit()
 
     @classmethod
     def clear_pitch(cls):
         Pitch.all_pitch.clear()
 
     @classmethod
-    def get_pitch(cls,uname):
-        response = []
-
-        for pitch in cls.all_pitch:
-                response.append(pitch)
-        return response
+    def get_pitch(cls,id):
+        pitch = Pitch.query.filter_by((pitch_id=id).all()
+        return pitch
