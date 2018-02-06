7d00c683157515de91e3f11bb1d790e7b486e6eb
VickyMutai <vicky.mutai96@gmail.com>
2018-02-06 06:23:26 +0300

    add drop down menu

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField,RadioField, TextAreaField, SubmitField,SelectField
 from wtforms.validators import Required
 
 class PitchForm(FlaskForm):
     name = StringField('Authors Name', validators=[Required()])
-    category = StringField('Pitch Category', validators=[Required()])
-    pitch = TextAreaField('Pitch', validators=[Required()])
+    category = SelectField('Pick Category here',choices=[('inspiration','Inspiration'),
+                                                        ('pick-up','Pick Up Lines'),
+                                                        ('interview','Interview Pitch'),
+                                                        ('product','Product Pitch'),
+                                                        ('promotion','Promotion Pitch')], validators=[Required()])
+    pitch = TextAreaField('Write your Pitch here', validators=[Required()])
     submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
